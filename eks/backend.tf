ab9bd2bdaac6fb3bd8ec1fa0d6407febff27d972
brams898 <bramha.official898@gmail.com>
2024-10-28 16:05:29 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-eaws-baket1"
+    bucket         = "my-eaws-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     #dynamodb_table = "Lock-Files"
